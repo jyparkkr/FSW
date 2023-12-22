443a28c169380f21d2696b0045227e427109ebf1
jyparkkr <jaeyoungpark.korea@gmail.com>
2023-12-22 14:31:43 +0900

    code update

--- algorithms/target_min.py
+++ algorithms/target_min.py
@@ -8,6 +8,8 @@ from cl_gym.algorithms import ContinualAlgorithm
 from cl_gym.algorithms.agem import AGEM
 from cl_gym.algorithms.utils import flatten_grads, assign_grads
 from torch.nn.functional import relu, avg_pool2d
+from algorithms.mean_std_min import Heuristic2
+
 import matplotlib.pyplot as plt
 
 import copy
@@ -21,7 +23,7 @@ def bool2idx(arr):
     return np.array(idx)
 
 
-class Heuristic2(ContinualAlgorithm):
+class Heuristic3(Heuristic2):
     # Implementation is partially based on: https://github.com/MehdiAbbanaBennani/continual-learning-ogdplus
     def __init__(self, backbone, benchmark, params, **kwargs):
         self.backbone = backbone
@@ -94,16 +96,15 @@ class Heuristic2(ContinualAlgorithm):
     def get_loss_grad(self, task_id, loader, current_set = False):
         criterion = self.prepare_criterion(task_id)
         device = self.params['device']
-        classwise_loss = {x:list() for x in self.benchmark.class_idx}
-        classwise_grad_dict = {x:list() for x in self.benchmark.class_idx}
+        sensitive_loss = {x:list() for x in range(2)}
+        sensitive_grad_dict = {x:list() for x in range(2)}
         new_grads = None
 
         for batch_idx, (inp, targ, t_id, *_) in enumerate(loader):
             # self.backbone.forward
             inp, targ, t_id  = inp.to(device), targ.to(device), t_id.to(device)
+            sen = _[0].to(device) # ADDED
             pred, embeds = self.forward_embeds(inp)
-            self.pred_shape = pred.shape[1]
-            self.embeds_shape = embeds.shape[1]
             criterion.reduction = "none"
             loss = criterion(pred, targ.reshape(-1))
             criterion.reduction = "mean"
@@ -113,40 +114,36 @@ class Heuristic2(ContinualAlgorithm):
             weight_grads = bias_expand * embeds.repeat(1, pred.shape[1])
             grads = torch.cat([bias_grads, weight_grads], dim=1)
 
-            for i, e in enumerate(targ):
-                classwise_loss[e.cpu().item()].append(loss[i])
-                classwise_grad_dict[e.cpu().item()].append(grads[i])
+            for i, e in enumerate(sen):
+                sensitive_loss[e.cpu().item()].append(loss[i])
+                sensitive_grad_dict[e.cpu().item()].append(grads[i])
 
             if current_set:
                 new_grads = grads if new_grads is None else torch.cat([new_grads, grads])
 
             self.backbone.zero_grad()
 
-        for x in self.benchmark.class_idx:
-            if len(classwise_loss[x]) == 0:
-                del classwise_loss[x]
-                del classwise_grad_dict[x]
-
-        if current_set == True and new_grads is None:
-            # new_grads = torch.empty((0, pred.shape[1]*(embeds.shape[1]+1)), device=device, dtype=torch.float32)
-            new_grads = torch.empty((0, self.pred_shape*(self.embeds_shape+1)), device=device, dtype=torch.float32)
-
-        return classwise_loss, classwise_grad_dict, new_grads
+        for x in range(2):
+            if len(sensitive_loss[x]) == 0:
+                del sensitive_loss[x]
+                del sensitive_grad_dict[x]
+        
+        return sensitive_loss, sensitive_grad_dict, new_grads
     
     def get_loss_grad_all(self, task_id):
         num_workers = self.params.get('num_dataloader_workers', 0)
 
-        classwise_loss, classwise_grad_dict, _ = self.get_loss_grad(task_id, self.episodic_memory_loader, current_set = False)
+        sensitive_loss, sensitive_grad_dict, _ = self.get_loss_grad(task_id, self.episodic_memory_loader, current_set = False)
         train_loader = self.benchmark.load(task_id, self.params['batch_size_train'], shuffle=False,
                                    num_workers=num_workers, pin_memory=True)[0]
         current_loss, current_grad_dict, new_grads = self.get_loss_grad(task_id, train_loader, current_set = True)
-        classwise_loss.update(current_loss)
-        classwise_grad_dict.update(current_grad_dict)
+        sensitive_loss.update(current_loss)
+        sensitive_grad_dict.update(current_grad_dict)
 
         losses = []
         grads = []
-        for k, v in classwise_loss.items():
-            v3 = classwise_grad_dict[k]
+        for k, v in sensitive_loss.items():
+            v3 = sensitive_grad_dict[k]
             losses.append(torch.stack(v).mean(dim=0).view(1, -1).detach().clone())
             grads.append(torch.stack(v3).mean(dim=0).view(1, -1).detach().clone())
 
@@ -160,7 +157,7 @@ class Heuristic2(ContinualAlgorithm):
             loss_matrix = losses.repeat(len(n_new_grads), 1)
             forget_matrix = torch.matmul(n_new_grads, torch.transpose(n_buffer_grads, 0, 1))
 
-        return loss_matrix, forget_matrix
+        return loss_matrix, forget_matrix, n_new_grads
 
 
 
@@ -178,7 +175,8 @@ class Heuristic2(ContinualAlgorithm):
             return self.benchmark.load(task_id, self.params['batch_size_train'],
                                     num_workers=num_workers, pin_memory=True)[0]
         
-        loss_matrix, forget_matrix = self.get_loss_grad_all(task_id)
+        loss_matrix, forget_matrix, n_new_grads = self.get_loss_grad_all(task_id)
+        print(f"{loss_matrix.shape=}")
 
         # current data selection
         accumulate_select_indexes = []
@@ -194,14 +192,21 @@ class Heuristic2(ContinualAlgorithm):
         # for debugging
         classwise_loss = []
 
-        for b in range(data_len):
+        for b in range(data_len-1):
             # inp, targ, t_id  = inp.to(device), targ.to(device), t_id.to(device)
             loss_matrix = loss_matrix - self.params['alpha'] * forget_matrix
             loss_mean = torch.mean(loss_matrix, dim=1, keepdim=True)
             loss_std = torch.std(loss_matrix, dim=1, keepdim=True)
 
-            select_ind = torch.argmin(loss_mean + loss_std, dim=0)
-            accumulate_sum.append(copy.deepcopy(loss_mean[select_ind].item() + loss_std[select_ind].item()))
+            loss_1 = loss_matrix[:,1]
+
+            # select_ind = torch.argmin(loss_mean + loss_std, dim=0)
+            select_ind = torch.argmin(loss_1, dim=0)
+            accumulate_sum.append(copy.deepcopy(loss_1[select_ind].item()))
+            # accumulate_sum.append(copy.deepcopy(loss_mean[select_ind].item() + loss_std[select_ind].item()))
+
+
+
             classwise_loss.append(loss_matrix[select_ind].view(-1).detach().clone().cpu().numpy())
 
             # num_dict[targets[select_ind.item()].item()] += 1
@@ -212,23 +217,21 @@ class Heuristic2(ContinualAlgorithm):
             del self.benchmark.seq_indices_train[task_id][select_ind.item()]
             del non_select_indexes[select_ind.item()]
 
-            loss_matrix, forget_matrix = self.get_loss_grad_all(task_id)
+            # loss_matrix, forget_matrix = self.get_loss_grad_all(task_id)
 
-            # best_buffer_losses = loss_matrix[select_ind].view(1,-1)
-            # loss_matrix = best_buffer_losses.repeat(len(n_new_grads)-1, 1)
+            best_buffer_losses = loss_matrix[select_ind].view(1,-1)
+            loss_matrix = best_buffer_losses.repeat(len(n_new_grads)-1, 1)
 
-            # n_new_grads = torch.cat((n_new_grads[:select_ind.item()], n_new_grads[select_ind.item()+1:]))
-            # forget_matrix = torch.cat((forget_matrix[:select_ind.item()], forget_matrix[select_ind.item()+1:]))
+            n_new_grads = torch.cat((n_new_grads[:select_ind.item()], n_new_grads[select_ind.item()+1:]))
+            forget_matrix = torch.cat((forget_matrix[:select_ind.item()], forget_matrix[select_ind.item()+1:]))
 
         # for debugging
-        save_path = f"./figs/alpha_{self.params['alpha']}"
+        save_path = f"./figs/sensitive_2/alpha_{self.params['alpha']}"
         os.makedirs(save_path, exist_ok=True)
         plt.plot(accumulate_sum)
         plt.savefig(f"{save_path}/tid_{task_id}_accumulate_loss.png")
         plt.clf()
 
-        for i in classwise_loss:
-            print(f"{i.shape}")
         classwise_loss = np.array(classwise_loss).T
         for i, e in enumerate(classwise_loss):
             plt.plot(e, label=i)
